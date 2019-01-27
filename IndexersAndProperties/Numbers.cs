2597dee3aee25dc32c937349657fa98c493077f1
DenisShulyak <denshufly@mail.ru>
2019-01-27 20:04:26 +0600

    IndexersAndProperties

---
+++ IndexersAndProperties/Numbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersAndProperties
+{
+    class Numbers
+    {
+        private int[] numbers;
+        public Numbers()
+        {
+            numbers = new int[10];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return numbers[index];
+            }
+            set
+            {
+                numbers[index] = value * value;
+            }
+        }
+    }
+}
---
+++ IndexersAndProperties/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersAndProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+            Console.WriteLine("Введите значение первого элемента массива: ");
+            numbers[0] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Первый элемент массива: " + numbers[0]);
+            Console.ReadKey();
+        }
+    }
+}
